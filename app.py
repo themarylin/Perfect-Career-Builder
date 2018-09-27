f338bc7c6f38bf89095a9c3394febdfdb9b3610b
Mary Lin <25216651+themarylin90@users.noreply.github.com>
2018-09-27 14:57:38 -0700

    working on querying data into json output

--- app.py
+++ app.py
@@ -36,35 +36,64 @@ def index():
     """Return the homepage."""
     return render_template("index.html")
 
+
 @app.route("/index.html")
 def home():
     """Return the homepage."""
     return render_template("index.html")
 
+
 @app.route("/phase1.html")
 def phase1():
-    """Return the Map.html."""
+    """Return choropleth map"""
     return render_template("phase1.html")
+
 
 @app.route("/phase2.html")
 def phase2():
     """Return the homepage."""
     return render_template("phase2.html")
 
+
 @app.route("/phase3.html")
 def phase3():
     """Return the homepage."""
     return render_template("phase3.html")
+
 
 @app.route("/phase4.html")
 def phase4():
     """Return the homepage."""
     return render_template("phase4.html")
 
-@app.route('/api/<int:id>', methods=['GET'])
-def get_one_sighting(id):
-    sighting = session.query(Sighting).filter(Sighting.id == id).one()
-    return render_template('single_sighting.html', sighting=sighting)
+# @TODO: fix this part of the code!!!!!!!~~~~~~~
+# this is where I gather data and slice it into my json file
+
+
+@app.route("/api/states")
+def api_sightings_geo_json():
+    sql = text("""SELECT state, COUNT(*) as cnt 
+FROM sightings
+WHERE country='US'
+GROUP BY state
+ORDER BY cnt DESC""")
+
+    result = engine.execute(sql)
+    states = {}
+    for row in result:
+        states[row[0]] = row[1]
+
+    with open('static/js/states.json') as data_file:
+        us_states_data = json.load(data_file)
+        # Append our state data to the US states geo JSON
+        for i, feature in enumerate(us_states_data['features']):
+            state = feature['id']
+            us_states_data['features'][i]['properties']['mean_annual_wage'] = states[state]
+            us_states_data['features'][i]['properties']['mean_hourly_wage'] = states[state]
+            us_states_data['features'][i]['properties']['jobs_in_1000'] = states[state]
+            us_states_data['features'][i]['properties']['mean_annual_wage'] = states[state]
+
+    return jsonify(us_states_data)
 
 
 if __name__ == "__main__":
