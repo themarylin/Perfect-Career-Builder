1c16e9accdff5d6caed6c7544ae3ee21c1765de2
Marvin <gomez.marvin323@gmail.com>
2018-10-01 00:48:49 -0700

    updating

--- bubbleChart/bubbles.js
+++ bubbleChart/bubbles.js
@@ -1,6 +1,6 @@
 (function(){
-  var width = 650,
-    height = 650;
+  var width = 800,
+    height = 800;
 
   var svg = d3.select("#chart")
   //   .select(".chart")
@@ -11,58 +11,36 @@
     .append("g")
     .attr("transform", "translate(0,0)")
 
-  var radiusScale = d3.scaleSqrt().domain([1, 800]).range([10, 80])
+  var radiusScale = d3.scaleSqrt().domain([758, 39303]).range([10, 70])
 
   //the simulation is a collection of forceSimulation
   //about where we want our circles to go
   //and how we want our circles to interact
-  var simulation = d3.forceSimulation(d3.json("/api/occupationstats").then(function (response) {
-            response.forEach(function (s) {
-                console.log(response);
-                if (s.attributes.STATE == stateName) {
-                    data.name = stateName;
-                    data.total_jobs = s.attributes.TOT_EMP;
-                    data.a_mean = s.attributes.A_MEAN;
-                    data.jobs_per_1000 = s.attributes.JOBS_1000;
-                    info.update(data);
-                };
-            });
-        });)
+  var simulation = d3.forceSimulation()
     //give force a name and define the force
     //strength should be a number between 0-1, play around with it
-    .force("x", d3.forceX(width / 2).strength(0.05))
-    .force("y", d3.forceY(height / 2).strength(0.05))
+    .force("x", d3.forceX(width / 2).strength(5))
+    .force("y", d3.forceY(height / 2).strength(5))
     //get circles to the middle
     //don't have them collide
     .force("collide", d3.forceCollide(function(d){
-      return radiusScale(d.sales)+1;
+      return radiusScale(d.frequency)+2;
     }))
 
   d3.queue()
-    .defer(d3.json("/api/occupationstats").then(function (response) {
-              response.forEach(function (s) {
-                  console.log(response);
-                  if (s.attributes.STATE == stateName) {
-                      data.name = stateName;
-                      data.total_jobs = s.attributes.TOT_EMP;
-                      data.a_mean = s.attributes.A_MEAN;
-                      data.jobs_per_1000 = s.attributes.JOBS_1000;
-                      info.update(data);
-                  };
-              });
-          });)
+    .defer(d3.csv,"job_common_words.csv")
     .await(ready)
 
   function ready (error, datapoints) {
 
-    var circles = svg.selectAll(".artist")
+    var circles = svg.selectAll(".word")
       .data(datapoints)
       .enter().append("circle")
-      .attr("class", "artist")
+      .attr("class", "word")
       //radius of circles
       .attr("r", function(d){
                            //size of cirlces
-        return radiusScale(d.sales);
+        return radiusScale(d.frequency);
       })
       .attr("fill", "lightblue")
       .on('click', function(d) {
@@ -81,8 +59,8 @@
     circles.on("mouseover", function(d) {
       toolTip.style("display", "block")
           .html(
-            `<strong>${(d.name)}<strong><hr>${d.decade}
-        medal(s) won`)
+            `<strong>${(d.name)}<strong><hr>${d.frequency}
+        Word Frequency`)
           .style("left", d3.event.pageX + "px")
           .style("top", d3.event.pageY + "px");
       })
