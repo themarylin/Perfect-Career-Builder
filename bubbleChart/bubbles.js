dc79c5729fa9446a19c1af29e3b90bea5e46a286
Marvin Gomez <gomez.marvin323@gmail.com>
2018-10-01 01:48:40 -0700

    Update bubbles.js

--- bubbleChart/bubbles.js
+++ bubbleChart/bubbles.js
@@ -1,6 +1,6 @@
 (function(){
-  var width = 500,
-    height = 500;
+  var width = 800,
+    height = 800;
 
   var svg = d3.select("#chart")
   //   .select(".chart")
@@ -18,9 +18,9 @@
   //and how we want our circles to interact
   var simulation = d3.forceSimulation()
     //give force a name and define the force
-    //stregth is be a number between 0-1, play around with it
-    .force("x", d3.forceX(width / 2).strength(0.05))
-    .force("y", d3.forceY(height / 2).strength(0.05))
+    //strength should be a number between 0-1, play around with it
+    .force("x", d3.forceX(width / 2).strength(5))
+    .force("y", d3.forceY(height / 2).strength(5))
     //get circles to the middle
     //don't have them collide
     .force("collide", d3.forceCollide(function(d){
@@ -28,7 +28,7 @@
     }))
 
   d3.queue()
-    .defer(d3.csv, "sales.csv")
+    .defer(d3.csv,"job_common_words.csv")
     .await(ready)
 
   function ready (error, datapoints) {
@@ -39,14 +39,38 @@
       .attr("class", "word")
       //radius of circles
       .attr("r", function(d){
-        return radiusScale(d.sales);
+                           //size of cirlces
+        return radiusScale(d.frequency);
       })
       .attr("fill", "lightblue")
       .on('click', function(d) {
         console.log(d)
       })
-      // .attr("cx", 100)
-      // .attr("cy", 300)
+      // // .attr("cx", 100)
+      // // .attr("cy", 300)
+
+    // Step 1: Append tooltip div
+    var toolTip = d3.select("body")
+      .append("div")
+      .style("display", "none")
+      .classed("tooltip", true);
+
+    // Step 2: Create "mouseover" event listener to display tooltip
+    circles.on("mouseover", function(d) {
+      toolTip.style("display", "block")
+          .html(
+            `<strong>${(d.word)}<strong><hr>${d.frequency}
+        words`)
+          .style("left", d3.event.pageX + "px")
+          .style("top", d3.event.pageY + "px");
+          d3.select(this).style("stroke", "#323232");
+      })
+        // Step 3: Create "mouseout" event listener to hide tooltip
+       .on("mouseout", function() {
+         toolTip.style("display", "none");
+         d3.select(this).style("stroke", "#e3e3e3");
+       });
+
 
     simulation.nodes(datapoints)
       .on('tick', ticked)
